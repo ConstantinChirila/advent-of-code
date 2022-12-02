365e8fd2514e1ebbefcf9f5b2f3e0d0baaca0c17
Constantin Chirila <constantin@diagrid.io>
2022-12-02 13:36:29 +0000

    More refactoring

--- app/page.jsx
+++ app/page.jsx
@@ -10,8 +10,37 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <br />
-          <a href="https://adventofcode.com/2022/">Advent of code challenge</a>
+          <Link href="https://adventofcode.com/" target="_blank">
+            Advent of code challenge
+          </Link>
         </h1>
+        <div className={styles.content}>
+          <p>
+            While leet code is{" "}
+            <Link
+              href="https://www.fullcontextdevelopment.com/qb/leetcode-considered-harmful"
+              target="_blank"
+            >
+              not my favorite thing
+            </Link>{" "}
+            to do, I do love doing the yearly{" "}
+            <Link href="https://adventofcode.com/" target="_blank">
+              advent of code challenge
+            </Link>
+            . I decided to do it, timed, in{" "}
+            <Link
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+              target="_blank"
+            >
+              Javascript
+            </Link>{" "}
+            and secondly try to do it in
+            <Link href="https://golang.org/" target="_blank">
+              Go
+            </Link>{" "}
+            in an attempt to learn Go lang as well.
+          </p>
+        </div>
         <table className={styles.table}>
           <thead>
             <tr>
