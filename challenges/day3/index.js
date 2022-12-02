89dc629b5a8e245246f8853699a7cd2b764644ed
Constantin Chirila <constantin@diagrid.io>
2022-12-02 15:01:11 +0000

    Prepare for next challenge

--- challenges/day3/index.js
+++ challenges/day3/index.js
@@ -1,11 +1,24 @@
 import { text } from "./input";
 
-export function day3() {
+function runChallenge() {
   // Your code here
 
   return {
     part1: 0,
     part2: 0,
-    time: 0, //minutes
+  };
+}
+
+export function day2() {
+  // Cached results as it can be expensive to run all the challenges at once.
+  return {
+    ...(!EXECUTE_ALL_CHALLENGES
+      ? runChallenge()
+      : {
+          part1: 13675,
+          part2: 14184,
+        }),
+    time: 23,
+    hasGo: true,
   };
 }
