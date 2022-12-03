756ce3515e8969fcdb5e736327becba09d25cacf
Constantin Chirila <constantin.chirila@gmail.com>
2022-12-03 22:25:22 +0000

    Day 3 challenge javascript

--- challenges/day3/index.js
+++ challenges/day3/index.js
@@ -1,25 +1,73 @@
 import { EXECUTE_ALL_CHALLENGES } from "../../config";
-import { text } from "./input";
+import { sum } from "../../utils";
+import { TEXT } from "./input";
+//get alphabet in array
+const CHARACTER_ORDER =
+  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
 function runChallenge() {
-  // Your code here
+  const backpacks = TEXT.trim()
+    .split("\n\n")
+    .map((str) => str.split("\n"))[0];
+
+  const priorityScore = backpacks.map((b) => {
+    const half = b.length / 2;
+    const firstHalf = b.slice(0, half);
+    const secondHalf = b.slice(half);
+
+    const commonLetters = firstHalf
+      .split("")
+      .filter((char) => secondHalf.includes(char));
+
+    return getPriority(commonLetters[0]);
+  });
+
+  const groupedBackpacks = getGroups(backpacks);
+  const priorityScorePart2 = groupedBackpacks.map((group) => {
+    const commonLetters = group[0]
+      .split("")
+      .filter((char) => group[1].includes(char) && group[2].includes(char));
+    return getPriority(commonLetters[0]);
+  });
 
   return {
-    part1: 0,
-    part2: 0,
+    part1: priorityScore.reduce(sum, 0),
+    part2: priorityScorePart2.reduce(sum, 0),
   };
+}
+
+function getPriority(item) {
+  const index = CHARACTER_ORDER.findIndex((char) => char === item);
+
+  return index + 1;
+}
+
+function getGroups(items) {
+  const groups = [];
+
+  let currentGroup = [];
+  for (const item of items) {
+    currentGroup.push(item);
+
+    if (currentGroup.length === 3) {
+      groups.push(currentGroup);
+      currentGroup = [];
+    }
+  }
+
+  return groups;
 }
 
 export function day3() {
   // Cached results as it can be expensive to run all the challenges at once. Used to present on the UI
   return {
-    ...(EXECUTE_ALL_CHALLENGES
+    ...(!EXECUTE_ALL_CHALLENGES
       ? runChallenge()
       : {
-          part1: 0,
-          part2: 0,
+          part1: 8185,
+          part2: 2817,
         }),
-    time: 0,
+    time: 28,
     hasGo: false,
   };
 }
