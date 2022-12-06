182b4b501428e9580186d21ff17974599bf186ba
Constantin Chirila <constantin@diagrid.io>
2022-12-06 11:00:19 +0000

    Finished day 6 challenge

--- challenges/day6/index.js
+++ challenges/day6/index.js
@@ -1,12 +1,27 @@
 import { EXECUTE_ALL_CHALLENGES } from "../../config";
-import { text } from "./input";
+import { TEXT } from "./input";
 
 function runChallenge() {
-  // Your code here
+  const chars = TEXT.split("");
+
+  let found1, found2;
+  chars.forEach((_, index) => {
+    const group1 = chars.slice(index, index + 4);
+    const set1 = new Set(group1);
+    const group2 = chars.slice(index, index + 14);
+    const set2 = new Set(group2);
+
+    if (set1.size === 4 && !found1) {
+      found1 = index + 4;
+    }
+    if (set2.size === 14 && !found2) {
+      found2 = index + 14;
+    }
+  });
 
   return {
-    part1: 0,
-    part2: 0,
+    part1: found1,
+    part2: found2,
   };
 }
 
@@ -16,10 +31,10 @@ export function day6() {
     ...(EXECUTE_ALL_CHALLENGES
       ? runChallenge()
       : {
-          part1: 0,
-          part2: 0,
+          part1: 1876,
+          part2: 2202,
         }),
-    time: 0,
+    time: 15,
     hasGo: false,
   };
 }
